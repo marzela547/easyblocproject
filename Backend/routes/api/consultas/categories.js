7f9575f652dc365726f253b7e1d60e3e90db0d69
kref2400 <kevinespinal0024@gmail.com>
2021-11-27 04:01:37 -0600

    update categorias

--- Backend/routes/api/consultas/categories.js
+++ Backend/routes/api/consultas/categories.js
@@ -2,19 +2,28 @@ var conn = require('../../../utils/conexion');
 var ObjectID = require('mongodb').ObjectId;
 var _db;
 
-class Categories{
-    categoriesColl =null;
-    constructor(){
+class Categories {
+    categoriesColl = null;
+    constructor() {
         this.initModel();
     }
-    async initModel(){
+    async initModel() {
         try {
-        _db = await conn.getDB();
-        this.categoriesColl = await _db.collection("categoria");
-        }catch(ex){
-        console.log(ex);
-        process.exit(1);
+            _db = await conn.getDB();
+            this.categoriesColl = await _db.collection('categoriax');
+        } catch (ex) {
+            console.log(ex);
+            process.exit(1);
         }
+    }
+
+    async updCategoria(Descripcion_Cat, id) {
+        // UPDATE SWOT set swotMeta = 'Nuevo Valor' where _id = 'aId';
+        let filter = { _id: new ObjectID(id) };
+        let result = await this.categoriesColl.updateOne(filter, {
+            $set: { Descripcion_Cat: Descripcion_Cat },
+        });
+        return result;
     }
 }
 
--- Backend/routes/api/consultas/index.js
+++ Backend/routes/api/consultas/index.js
@@ -2,24 +2,37 @@ var express = require('express');
 var router = express.Router();
 var NotesDao = require('./notes');
 var notes = new NotesDao();
-
+var CategoriesDao = require('./categories');
+var Categories = new CategoriesDao();
 
 //**********Consultar 1 dato**********************************
 
-router.get('/byid/:id', async (req, res, next)=>{
-  try {
-    const {id} = req.params;
-    const oneNoteEntry = await notes.getById(id);
-    return res.status(200).json(oneNoteEntry);
-    console.log("Hola papis");
-  } catch (ex) {
-    console.log(ex);
-    return res.status(500).json({ msg: "Error al procesar petición" });
-  }
-}); 
+router.get('/byid/:id', async(req, res, next) => {
+    try {
+        const { id } = req.params;
+        const oneNoteEntry = await notes.getById(id);
+        return res.status(200).json(oneNoteEntry);
+        console.log('Hola papis');
+    } catch (ex) {
+        console.log(ex);
+        return res.status(500).json({ msg: 'Error al procesar petición' });
+    }
+});
 /********************************************************** */
 
-
-
+/********Consultas Kevin***********************************************/
+router.put('/updCategoria/:id', async(req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { Descripcion_Cat } = req.body;
+        const result = await note.updCategoria(id, Descripcion_Cat);
+        console.log(result);
+        res.status(200).json({ msg: 'Modificado OK' });
+    } catch (ex) {
+        console.log(ex);
+        return res.status(500).json({ msg: 'Error al procesar petición' });
+    }
+});
+/******************************************************************** */
 
 module.exports = router;
