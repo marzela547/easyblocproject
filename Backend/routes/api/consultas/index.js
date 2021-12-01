beaf1633191d82fbe62268728bd03ae5fbb9bf6d
Franciscogz <65878436+Franciscogz@users.noreply.github.com>
2021-12-01 14:24:18 -0600

    Revert "Merge branch 'main' of https://github.com/marzela547/easyblocproject"
    This reverts commit cf95bc6e2808c28dc364c9a47b34de922af038a7, reversing
    changes made to 583e1f83477be6bafc23a628bb46d4470403214c.

--- Backend/routes/api/consultas/index.js
+++ Backend/routes/api/consultas/index.js
@@ -2,89 +2,39 @@ var express = require('express');
 var router = express.Router();
 var NotesDao = require('./notes');
 var notes = new NotesDao();
-var CategoriesDao = require('./categories');
-var Categories = new CategoriesDao();
 var CategeroisDao = require('./categories');
 var categories = new CategeroisDao();
 
-//agregar nueva nota*********
-router.post('/new', async(req, res, next) => {
-    try {
-        const {
-            descripcion_not,
-            categoria_cat,
-            titulo_not,
-            imagenes_not,
-            descripcion_not,
-            correo_usu
 
-        } = req.body;
-        const swotMetaArray = swotMeta.split('|');
-        // validaciones
-        const result = await Swot.addNew(descripcion_not, categoria_cat, titulo_not, imagenes_not, descripcion_not, correo_usu, req.nota._id);
-        console.log(result);
-        res.status(200).json({ msg: "Agregado Satisfactoriamente" });
-    } catch (ex) {
-        console.log(ex);
-        return res.status(500).json({ msg: "Error al procesar petición" });
-    }
-}); // /new
+//**********Consultar 1 dato**********************************
 
-//********consultar notas por categoria */
-router.get('/bycategoria/:categoria', async(req, res, next) => {
-            try {
-                const { categoria } = req.params;
+router.get('/byid/:id', async (req, res, next)=>{
+  try {
+    const {id} = req.params;
+    const oneNoteEntry = await notes.getById(id);
+    return res.status(200).json(oneNoteEntry);
+    console.log("Hola papis");
+  } catch (ex) {
+    console.log(ex);
+    return res.status(500).json({ msg: "Error al procesar petición" });
+  }
+}); 
+/********************************************************** */
 
-                const swots = await Swot.getBycategoria(type, req._id);
-                return res.status(200).json(swots);
-            } catch (ex) {
-                console.log(ex);
-                return res.status(500).json({ msg: "Error al procesar petición" });
-            }
+//**********Eliminar 1 dato**********************************
 
-            //**********Consultar 1 dato**********************************
+router.delete('/delete/:id', async (req, res, next)=>{
+  try {
+    const {id} = req.params;
+    const result = await categories.deleteById(id);
+    console.log(result);
+    return res.status(200).json({"msg":"Eliminado OK"});
+  } catch (ex) {
+    console.log(ex);
+    return res.status(500).json({ msg: "Error al procesar petición" });
+  }
+});
+/********************************************************** */
 
-            router.get('/byid/:id', async(req, res, next) => {
-                try {
-                    const { id } = req.params;
-                    const oneNoteEntry = await notes.getById(id);
-                    return res.status(200).json(oneNoteEntry);
-                    console.log('Hola papis');
-                } catch (ex) {
-                    console.log(ex);
-                    return res.status(500).json({ msg: 'Error al procesar petición' });
-                }
-            });
-            /********************************************************** */
 
-            /********Consulta MODIFICAR UNA CATEGORIA***********************************************/
-            router.put('/updCategoria/:id', async(req, res, next) => {
-                try {
-                    const { id } = req.params;
-                    const { Descripcion_Cat } = req.body;
-                    const result = await categories.updCategoria(id, Descripcion_Cat);
-                    console.log(result);
-                    res.status(200).json({ msg: 'Modificado OK' });
-                } catch (ex) {
-                    console.log(ex);
-                    return res.status(500).json({ msg: 'Error al procesar petición' });
-                }
-            });
-            /******************************************************************** */
-
-            //**********Eliminar 1 dato**********************************
-
-            router.delete('/delete/:id', async(req, res, next) => {
-                try {
-                    const { id } = req.params;
-                    const result = await categories.deleteById(id);
-                    console.log(result);
-                    return res.status(200).json({ msg: 'Eliminado OK' });
-                } catch (ex) {
-                    console.log(ex);
-                    return res.status(500).json({ msg: 'Error al procesar petición' });
-                }
-            });
-            /********************************************************** */
-
-            module.exports = router;
+module.exports = router;
--- Backend/routes/api/security/index.js
+++ Backend/routes/api/security/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 let router = express.Router();
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
 let SecModelClass = require('./sec.model.js');
 let SecModel = new SecModelClass();
 const mailSender = require('../../../utils/mailer');
@@ -59,84 +59,61 @@ passport.use(new KakaoStrategy({
 ))
 */
 //--------------------------------------------------------------------
-router.post('/login', async(req, res, next) => {
-    try {
-        const { email, pswd } = req.body;
-        let userLogged = await SecModel.getByEmail(email);
-        if (userLogged) {
-            const isPswdOk = await SecModel.comparePassword(
-                pswd,
-                userLogged.contrasena_usu
-            );
-
-            if (isPswdOk) {
-                // podemos validar la vigencia de la contraseña
-                delete userLogged.contrasena_usu;
-                delete userLogged.oldpasswords;
-                delete userLogged.lastlogin;
-                delete userLogged.lastpasswordchange;
-                delete userLogged.passwordexpires;
-                let payload = {
-                    jwt: jwt.sign({
-                            email: userLogged.email,
-                            _id: userLogged._id,
-                            roles: userLogged.roles,
-                        },
-                        process.env.JWT_SECRET, { expiresIn: '1d' }
-                    ),
-                    user: userLogged,
-                };
-                return res.status(200).json(payload);
-            }
-        }
-    } catch (ex) {
-        console.log(ex);
-        res.status(500).json({ msg: 'Error' });
-    }
-});
-
-router.get('/email', async(req, res, next) => {
-    try {
-        const { email, pswd } = req.body;
-        let userLogged = await SecModel.getByEmail(email);
+router.post('/login', async (req, res, next)=>{
+  try {
+    const {email, pswd} = req.body;
+    //Validar los datos
+    let userLogged = await SecModel.getByEmail(email);
+    if (userLogged) {
+      const isPswdOk = await SecModel.comparePassword(pswd, userLogged.password);
+      if (isPswdOk) {
+        // podemos validar la vigencia de la contraseña
+        delete userLogged.password;
+        delete userLogged.oldpasswords;
+        delete userLogged.lastlogin;
+        delete userLogged.lastpasswordchange;
+        delete userLogged.passwordexpires;
         let payload = {
-            email: userLogged.nombre_usu,
-            tel: userLogged.telefono_usu,
+          jwt: jwt.sign(
+            {
+              email: userLogged.email,
+              _id: userLogged._id,
+              roles: userLogged.roles
+            },
+            process.env.JWT_SECRET,
+            {expiresIn:'1d'}
+          ),
+          user: userLogged
         };
         return res.status(200).json(payload);
-    } catch (ex) {
-        console.log(ex);
-        return res.status(500).json({ msg: 'Error al procesar petición' });
+      }
     }
+    console.log({email, userLogged});
+    return res.status(400).json({msg: "Credenciales no son Válidas"});
+  }catch (ex){
+    console.log(ex);
+    res.status(500).json({"msg":"Error"});
+  }
 });
 
-router.post('/signin', async(req, res, next) => {
-    try {
-        const { name, lastname, phone, email, password } = req.body;
-        let userAdded = await SecModel.createNewUser(
-            name,
-            lastname,
-            phone,
-            email,
-            password
-        );
-        delete userAdded.password;
-        console.log(userAdded);
-        res.status(200).json({ msg: 'Usuario creado correctamente' });
-    } catch (ex) {
-        res.status(500).json({ msg: 'Error' });
-    }
+router.post('/signin', async (req, res, next) => {
+  try {
+    const { name, lastname, phone, email, password} = req.body;
+    let userAdded = await SecModel.createNewUser( name, lastname, phone, email, password);
+    delete userAdded.password;
+    console.log(userAdded);
+    res.status(200).json({"msg":"Usuario creado correctamente"});
+  } catch (ex) {
+    res.status(500).json({ "msg": "Error" });
+  }
 });
 
-router.get(
-    '/kakao/getinfo',
-    passport.authenticate('kakao', { failureRedirect: '/' }),
-    (req, res) => {
-        res.status(200).json({ msg: '¡Eureka! Funciona, Kakao' });
-    }
-);
+router.get('/kakao/getinfo',passport.authenticate('kakao', { failureRedirect: '/' }), (req, res)=>{
+  res.status(200).json({"msg":"¡Eureka! Funciona, Kakao"});
+});
 
-router.get('/auth/kakao', passport.authenticate('kakao'));
+router.get('/auth/kakao',
+  passport.authenticate('kakao'));
 /*
 router.get('/oauth/kakao/callback', 
   passport.authenticate('kakao', { failureRedirect: '/' }),
@@ -147,19 +124,18 @@ router.get('/oauth/kakao/callback',
 */
 //----------------------GITHUB
 
-router.get('/github/getinfo', (req, res) => {
-    res.status(200).json({ msg: '¡Eureka! Funciona, GitHub' });
+router.get('/github/getinfo', (req, res)=>{
+  res.status(200).json({"msg":"¡Eureka! Funciona, GitHub"});
 });
-router.get('/auth/github', passport.authenticate('github'));
+router.get('/auth/github',
+  passport.authenticate('github'));
 
-router.get(
-    '/auth/github/callback',
-    passport.authenticate('github', { failureRedirect: '/' }),
-    function(req, res) {
-        // Successful authentication, redirect home.
-        res.status(401);
-    }
-);
+router.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.status(401);
+  });
 //-------------------------------
 let cache = '';
 router.post('/recovery', async (req, res, next) => {
@@ -187,7 +163,7 @@ router.post('/passrecovery', async(req, res, next)=>{
     mailSender(usu.correo_usu, "Recuperación de contraseña", "Código de verificación: "+codigo);
   }
 
-    res.status(200).json({ msg: 'Correo enviado correctamente' });
-});
+  res.status(200).json({"msg":"Correo enviado correctamente"});
+})
 
 module.exports = router;
